1baa9c293ad69631cff7a1887d453aa1830d354f
eungchan-kang <yeschan119@gmail.com>
2023-10-13 22:26:14 +0900

    s

--- webtoon/lib/main.dart
+++ webtoon/lib/main.dart
@@ -6,7 +6,7 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  //모든 위젯을 key를 가지고 있고, 이 key는 flutter가 웨젯을 찾을 때 사용하는 ID 이다.
+  //모든 위젯은 key를 가지고 있고, 이 key는 flutter가 웨젯을 찾을 때 사용하는 ID 이다.
   const MyApp({super.key});
 
   @override
